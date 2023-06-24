d0173213f7414374428b18a5e1a1290e5ff046b6
Anna <34191811+404unf@users.noreply.github.com>
2023-06-23 21:51:56 -0700

    Sending and Receiving data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Add your code here
+
+
+
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json",
+//     },
+//     body: JSON.stringify({
+//         dogName: "Byron",
+//         dogBreed: "Poodle",
+//     }),
+// };
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (object) {
+//         console.log(object);
+//     });
+
+
+function submitData(userName,userEmail){
+    return fetch('http://localhost:3000/users',{
+        'method':'POST',
+        'headers':{
+            'Content-Type':'application/json',
+            'Accept':'application/json'
+        },
+        'body':JSON.stringify({'name':userName,'email':userEmail})
+    })
+        .then(response=>response.json())
+        .then(object=>document.querySelector('body').append(object.id))
+        .catch(error=>document.querySelector('body').append(error.message))
+}
